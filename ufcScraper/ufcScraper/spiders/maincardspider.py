b6d6c602544947f8e1fc847d28d184c66afa337d
Yu <ay3xqa@virginia.edu>
2024-08-01 20:42:35 -0400

    add scrape for flag, odds, record, rank

--- ufcScraper/ufcScraper/spiders/maincardspider.py
+++ ufcScraper/ufcScraper/spiders/maincardspider.py
@@ -23,13 +23,48 @@ class MaincardspiderSpider(scrapy.Spider):
         identifiers = [x.css("div.c-listing-fight").attrib["data-fmid"] for x in main_card_fights]
         res = []
         for fight in main_card_fights:
-            res.append(fight.css("div.c-listing-fight__corner-name--red a").attrib["href"])
-            res.append(fight.css("div.c-listing-fight__corner-name--blue a").attrib["href"])
+            #div.c-listing-fight__country-text::text
+            f1_flag = fight.css("div.c-listing-fight__country--red img").attrib["src"]
+            f2_flag = fight.css("div.c-listing-fight__country--blue img").attrib["src"]
+            ranks = fight.css("div.c-listing-fight__corner-rank")
+            f1_rank = ranks[0].css("span::text").get()
+            f2_rank = ranks[1].css("span::text").get()
+            if f1_rank:
+                if "C" not in f1_rank:
+                    f1_rank = f1_rank[1:]
+            else:
+                f1_rank = ""
+            if f2_rank:
+                if "C" not in f2_rank:
+                    f2_rank = f2_rank[1:]
+            else:
+                f2_rank = ""
+            odds = fight.css("span.c-listing-fight__odds-amount")
+            f1_ml = odds[0].css("::text").get()
+            f2_ml = odds[1].css("::text").get()
+
+
+            f1_info = {"f1_flag": f1_flag,
+                       "f1_rank": f1_rank,
+                       "f1_ml": f1_ml}
+            
+            f2_info = {"f2_flag": f2_flag,
+                        "f2_rank": f2_rank,
+                        "f2_ml": f2_ml}
+
+            # fight.css("div.c-listing-fight__corner-rank span")
+            res.append((fight.css("div.c-listing-fight__corner-name--red a").attrib["href"],f1_info))
+            res.append((fight.css("div.c-listing-fight__corner-name--blue a").attrib["href"],f2_info))
+
+            # res.append(fight.css("div.c-listing-fight__corner-name--red a").attrib["href"])
+            # res.append(fight.css("div.c-listing-fight__corner-name--blue a").attrib["href"])
         for i in range(0, len(res), 2):
             if i + 1 < len(res):
-                f1 = self.fetch_and_parse_fight(res[i], "f1_")
-                f2 = self.fetch_and_parse_fight(res[i + 1], "f2_")
+                f1 = self.fetch_and_parse_fight(res[i][0], "f1_")
+                f2 = self.fetch_and_parse_fight(res[i + 1][0], "f2_")
                 f1.update(f2)
+                f1.update(res[i][1])
+                f1.update(res[i+1][1])
                 yield f1
         # for fight in main_card_fights:
         #     f1_name = fight.css("div.c-listing-fight__corner-name--red span.c-listing-fight__corner-given-name::text").get() + fight..css("div.c-listing-fight__corner-name--red span.c-listing-fight__corner-given-name::text").get()
@@ -51,13 +86,17 @@ class MaincardspiderSpider(scrapy.Spider):
         weight = f"{float(weight):.1f}"
 
         reach = selector.css("div.c-bio__text")[-2].css("::text").get()
-        reach = f'{reach}"'
+        reach = f'{float(reach):.1f}"'
 
+        record_line = selector.css("p.hero-profile__division-body::text").get()
+        record_line = record_line.split(" ")
+        record = record_line[0]
         return {
             prefix+"name": name,
             prefix+"age": age,
             prefix+"height": height,
             prefix+"weight": weight,
             prefix+"reach": reach,
-            prefix+"image": img
+            prefix+"image": img,
+            prefix+"record": record
         } 
